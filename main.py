3e41c5b2aeb51111c98620a07079bc41b308e0f0
andy <ritmorica@gmail.com>
2023-12-11 14:02:43 +0300

    fix: plus token

---
+++ main.py
@@ -0,0 +1,40 @@
+﻿import os
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+output_file_name = 'iptv.m3u'
+
+files = [f for f in os.listdir(current_path) if os.path.isfile(os.path.join(current_path, f)) and f != output_file_name and f.endswith('.m3u')]
+
+
+# Имя входного файла
+input_file_name = files[0]
+
+output_file_name = os.path.join(current_path, output_file_name)
+
+# Открываем входной файл для чтения
+with open(input_file_name, 'r', encoding='utf-8') as input_file:
+    # Читаем все строки файла
+    lines = input_file.readlines()
+
+# Открываем выходной файл для записи
+with open(output_file_name, 'w', encoding='utf-8') as output_file:
+    # Переписываем строки, содержащие слово "СССР"
+    for idx, line in enumerate(lines):
+        try:
+
+            if any(i in lines[idx + 2].lower() for i in ['ссср', 'ussr']):
+                line = '#EXTGRP:Советское кино\n'
+
+            elif any(i in lines[idx + 2].lower() for i in ['comedy', 'комеди']):
+                line = '#EXTGRP:Комедии\n'
+
+            elif any(i in lines[idx + 2].lower() for i in ['marvel']):
+                line = '#EXTGRP:Комедии\n'
+
+
+            output_file.write(line)
+
+        except:
+            continue
+
+print(f'Изменения сохранены в файле: {output_file_name}')
